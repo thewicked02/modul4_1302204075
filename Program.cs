c8e9a43cca67fffc37e5edd1afb12caded7840ae
rodo <rodonatanael02@gmail.com>
2022-03-19 16:55:39 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace modul4_1302204075
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleDataBase<Double> sdb = new SimpleDataBase<Double>();
+
+            sdb.AddNewData(13); sdb.AddNewData(02); sdb.AddNewData(20);
+            sdb.PrintAllData();
+        }
+    }
+}
---
+++ SimpleDataBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302204075
+{
+    internal class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            this.storedData = new List<T>();
+            this.inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T data)
+        {
+            storedData.Add(data);
+            inputDates.Add(DateTime.Now);
+        }
+
+        public void PrintAllData()
+        {
+            foreach (var a in storedData.Zip(inputDates, Tuple.Create))
+            {
+                Console.Write(a.Item1 + " "); Console.WriteLine(a.Item2);
+            }
+        }
+    }
+}
